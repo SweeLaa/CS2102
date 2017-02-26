0c179f740f6850d59c331ce7062f7f6a72d84bcd
Sharmaine Swee <sweesl1993@gmail.com>
2017-02-26 13:04:39 +0800

    First commit for view projects page

---
+++ viewproject.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>FundMyLife - Crowd Fund Campaigns</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Fund My Life</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#services">Manage</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Projects</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Log In</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    
+    <br>
+
+    <section style="background-color:#F7F5F2;">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h1 style="color:#F05F40; font-size:100px">Discover Projects</h1>
+                    <h2>For like-minded individuals</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <br>
+
+<div role="tabpanel">
+            <div class="col-sm-3">
+                <ul class="nav nav-pills brand-pills nav-stacked" role="tablist">
+                    <li role="presentation" class="brand-nav active"><a href="#tab_artcraft" aria-controls="tab_artcraft" role="tab" data-toggle="tab">Arts & Crafts</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_design" aria-controls="tab_design" role="tab" data-toggle="tab">Design</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_fashion" aria-controls="tab_fashion" role="tab" data-toggle="tab">Fashion</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_filmvid" aria-controls="tab_filmvid" role="tab" data-toggle="tab">Film & Video</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_games" aria-controls="tab_games" role="tab" data-toggle="tab">Games</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_music" aria-controls="tab_music" role="tab" data-toggle="tab">Music</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_photography" aria-controls="tab_photography" role="tab" data-toggle="tab">Photography</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_technology" aria-controls="tab_technology" role="tab" data-toggle="tab">Technology</a></li>
+                    <li role="presentation" class="brand-nav"><a href="#tab_publishjournal" aria-controls="tab_publishjournal" role="tab" data-toggle="tab">Publishing & Journalism</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-9">
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="tab_artcraft">
+                        <p>
+                            Art & Craft Projects here, consectetur adipiscing elit. Mauris tincidunt feugiat lorem, at ornare tellus faucibus ut.
+                            Curabitur eget metus dapibus nibh laoreet lacinia eget id metus. Mauris quis convallis elit, ac convallis augue. 
+                            Pellentesque ornare cursus nibh quis fermentum. Morbi faucibus at tortor sed volutpat. Etiam at ex molestie turpis aliquam auctor.
+                            Nulla consequat tristique augue, vel venenatis massa fringilla sit amet. Morbi egestas turpis facilisis sem faucibus finibus.
+                        </p>
+                        <p>
+                            Nam sodales magna eget nulla interdum gravida. Aenean cursus magna vel lorem eleifend, vel eleifend massa rhoncus. 
+                            Duis accumsan vehicula ultricies. Proin tincidunt blandit congue. Curabitur semper odio ut malesuada dapibus.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_design">
+                        <p>
+                            Design related Projects here, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. 
+                            Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
+                            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+                            Qui animated corpse, cricket bat max brucks terribilem incessu zomby. 
+                        </p>
+                        <p>
+                            The voodoo sacerdos flesh eater, suscitat mortuos comedere 
+                            carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+                            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_fashion">
+                        <p>
+                            Fashion related projects here, consectetuer pizzle elizzle. Nullizzle yo velizzle, check it out volutpizzle, quis, gravida vel, yo.
+                            Ma nizzle eget tortor. Sizzle eros. My shizz izzle dolizzle gizzle turpis tempizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle.
+                            Maurizzle pellentesque nibh izzle own yo'. Check it out in tortor. Pellentesque fizzle rhoncizzle nisi. 
+                        </p>
+                        <p>
+                            In hac habitasse platea dictumst. Shizzlin dizzle dapibus. You son of a bizzle tellizzle urna, pretizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle,
+                            ghetto ac, check it out vitae, nunc. Shizzlin dizzle suscipizzle. Integizzle sempizzle velit sizzle dizzle.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_filmvid">
+                        <p>
+                            Film & Video projects here, Collaboratively administrate empowered markets via plug-and-play networks. 
+                            Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without 
+                            revolutionary ROI.
+                        </p>
+                        <p>
+                            Efficiently unleash cross-media information without cross-media value. 
+                            Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar 
+                            solutions without functional solutions.
+                        </p>
+                        <p>
+                            Completely synergize resource sucking relationships via premier niche markets. Professionally cultivate 
+                            one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service 
+                            for state of the art customer service.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_games">
+                        <p>
+                            Anything about games, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. 
+                            Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
+                            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+                            Qui animated corpse, cricket bat max brucks terribilem incessu zomby. 
+                        </p>
+                        <p>
+                            The voodoo sacerdos flesh eater, suscitat mortuos comedere 
+                            carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+                            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_music">
+                        <p>
+                            Music is the soul, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. 
+                            Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
+                            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+                            Qui animated corpse, cricket bat max brucks terribilem incessu zomby. 
+                        </p>
+                        <p>
+                            The voodoo sacerdos flesh eater, suscitat mortuos comedere 
+                            carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+                            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_photography">
+                        <p>
+                            Photography projects, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. 
+                            Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
+                            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+                            Qui animated corpse, cricket bat max brucks terribilem incessu zomby. 
+                        </p>
+                        <p>
+                            The voodoo sacerdos flesh eater, suscitat mortuos comedere 
+                            carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+                            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_technology">
+                        <p>
+                            Technology projects here, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. 
+                            Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
+                            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+                            Qui animated corpse, cricket bat max brucks terribilem incessu zomby. 
+                        </p>
+                        <p>
+                            The voodoo sacerdos flesh eater, suscitat mortuos comedere 
+                            carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+                            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="tab_publishjournal">
+                        <p>
+                            Publishing & Journalism, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. 
+                            Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
+                            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+                            Qui animated corpse, cricket bat max brucks terribilem incessu zomby. 
+                        </p>
+                        <p>
+                            The voodoo sacerdos flesh eater, suscitat mortuos comedere 
+                            carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+                            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+    
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+
+</body>
+
+</html>
